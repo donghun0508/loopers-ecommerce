83397d8aa0de8732a6e1a0f4cbe13a163b88258e
donghun0508 <alswn4516@gmail.com>
2025-07-16 01:17:56 +0900

    feat: 회원 가입 시 레이스 컨디션 방지를 위한 트랜잭션 추가
    - UserFacade.signUp()에 @Transactional 어노테이션 적용
    - 중복 검사와 회원 생성의 원자성 보장

--- apps/commerce-api/src/main/java/com/loopers/application/user/UserFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/user/UserFacade.java
@@ -8,6 +8,7 @@ import com.loopers.support.error.user.UserAlreadyExistsException;
 import com.loopers.support.error.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Component
@@ -15,6 +16,7 @@ public class UserFacade {
 
     private final UserService userService;
 
+    @Transactional
     public UserInfo signUp(UserCommand.Create command) {
         validateDuplicateUserId(command);
         User savedUser = userService.create(command);
