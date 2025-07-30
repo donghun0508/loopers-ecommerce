8fa457dc1c558d82ddd87eaed11997df311e3cc3
donghun0508 <alswn4516@gmail.com>
2025-07-30 23:47:40 +0900

    remove: 불필요한 임포트문 제거

--- apps/commerce-api/src/test/java/com/loopers/domain/order/OrderTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/order/OrderTest.java
@@ -5,17 +5,14 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import com.loopers.domain.fixture.OrderCommandFixture;
 import com.loopers.domain.fixture.TestArgumentProvider;
-import com.loopers.domain.order.OrderCommand.Create;
 import com.loopers.domain.order.OrderCommand.Create.OrderItem;
 import com.loopers.environment.annotations.UnitTest;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 
 @UnitTest
