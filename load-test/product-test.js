29033398e8646fdaea62c57d061284d07c3f8b7e
donghun0508 <alswn4516@gmail.com>
2025-08-14 19:08:49 +0900

    document: 부하테스트 스크립트 추가

---
+++ load-test/product-test.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { baseline } from './scenarios/baseline.js';
+import { rampup } from './scenarios/rampup.js';
+import { spike } from './scenarios/spike.js';
+import { stress } from './scenarios/stress.js';
+
+const BASE = 'http://localhost:8080';
+
+const SCENARIO = __ENV.SCENARIO || 'baseline';
+
+const scenarios = {
+  baseline: baseline,
+  rampup: rampup,
+  spike: spike,
+  stress: stress,
+};
+
+export const options = {
+  scenarios: scenarios[SCENARIO],
+};
+
+// export default function () {
+//   const sort = 'LIKES_DESC';
+//   const url = `${BASE}/api/v1/products?sort=${sort}`;
+//   http.get(url);
+// }
+
+export default function () {
+  const url = `${BASE}/api/v1/test`;
+  http.get(url);
+}
